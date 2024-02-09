2eb243d38aca5fb7b51de7448e714bd7bc35f7e8
Philip Kim <filshouse56@gmail.com>
2024-02-08 22:42:27 -0500

    Triangle Area Completed

--- Module_1_Review/Program.cs
+++ Module_1_Review/Program.cs
@@ -1,20 +1,2 @@
 ﻿using System;
 
-public class CircleCalculator
-{
-    
-    public static double CalculateCircleArea(double radius)
-    {
-        
-        double area = Math.PI * Math.Pow(radius, 2);
-        return area;
-    }
-
-    
-    public static void Main(string[] args)
-    {
-        double radius = 5.0;
-        double area = CalculateCircleArea(radius);
-        Console.WriteLine($"Area of the circle with radius {radius} is: {area}");
-    }
-}
---
+++ Module_1_Review/ShapeCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_1_Review
+{
+    public class ShapeCalculator
+    {
+
+        public static double CalculateCircleArea(double radius)
+        {
+
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+
+
+        public static void CirTest(string[] args)
+        {
+            double radius = 5.0;
+            double area = CalculateCircleArea(radius);
+            Console.WriteLine($"Area of the circle with radius {radius} is: {area}");
+        }
+
+
+
+
+        public static double CalculateTriangleArea(double baseLength, double height)
+        {
+
+            double area = 0.5 * baseLength * height;
+            return area;
+        }
+
+
+        public static void TriTest(string[] args)
+        {
+            double baseLength = 5.0;
+            double height = 8.0;
+            double area = CalculateTriangleArea(baseLength, height);
+            Console.WriteLine($"Area of the triangle with base {baseLength} and height {height} is: {area}");
+        }
+    }
+}
